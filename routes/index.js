d992965798065ccd01a6489953faec5442ae04a1
Jaredshongwe <jaredshongwe@gmail.com>
2024-02-02 11:14:36 +0200

    Added authentication , Login and Logout functionality.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 router.use('/', require('./swagger'));
-router.get('/', (req, res) => {
+/*router.get('/', (req, res) => {
     //#swagger.tags=['Hello Friend']
     res.send(`Hello Friend, My name is Jared. Welcome to the Books Project home page`);
-});
+});*/
 
 router.use('/books', require('./books'));
+
+//Login and Logout
+
+router.get('/login', passport.authenticate('github'), (req, res) => { });
+router.get('/logout', function (req, res, next) {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    })
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const mongodb = require('./data/database');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+    .use(session({
+        secret: "secret",
+        resave: false,
+        saveUninitialized: true,
+    }))
+    .use(passport.initialize())
+    .use(passport.session());
 
 // CORS middleware
 app.use((req, res, next) => {
@@ -18,6 +28,36 @@ app.use((req, res, next) => {
 
 // Routes
 app.use('/', require('./routes'));
+
+//Github auth
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+},
+    function (accessToken, refreshToken, profile, done) {
+        return done(null, profile)
+    }));
+
+
+passport.serializeUser((user, done) => {
+    done(null, user)
+});
+passport.deserializeUser((user, done) => {
+    done(null, user)
+});
+
+
+app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out') })
+
+app.get('/github/callback', passport.authenticate('github', {
+    failureRedirect: '/api-docs', session: false
+}),
+    (req, res) => {
+        req.session.user = req.user;
+        res.redirect('/')
+    })
+
 
 // Error handling
 process.on('uncaughtException', (err, origin) => {
